458befa6e5f37614b00f4f638d0fd6869c043812
Madan Joshi <mjoshi8@sapient.com>
2019-02-01 11:25:33 +0530

    people profile app

---
+++ app/.next/static/webpack/static/runtime/main.js.462bd1e5fe0214c77888.hot-update.js
@@ -0,0 +1,16 @@
+webpackHotUpdate("static/runtime/main.js",[
+/* 0 */
+/*!*************************************************************************************************************!*\
+  !*** multi ./static/libs/polyfills.js ./static/libs/polyfills.js ../node_modules/next/dist/client/next-dev ***!
+  \*************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(/*! /Users/madjoshi/Initiatives/people-profiling/universal-react/app/static/libs/polyfills.js */"./static/libs/polyfills.js");
+__webpack_require__(/*! /Users/madjoshi/Initiatives/people-profiling/universal-react/app/static/libs/polyfills.js */"./static/libs/polyfills.js");
+module.exports = __webpack_require__(/*! /Users/madjoshi/Initiatives/people-profiling/universal-react/node_modules/next/dist/client/next-dev */"../node_modules/next/dist/client/next-dev.js");
+
+
+/***/ })
+])
+//# sourceMappingURL=main.js.462bd1e5fe0214c77888.hot-update.js.map
